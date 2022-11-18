291c26952a409c12239284baf6b24ec1a817c141
Antoinne Williams <antoinne.lamar@gmail.com>
2022-11-18 11:21:54 -0500

    Added vars configuration to set global variables

--- vars/setQaVars.groovy
+++ vars/setQaVars.groovy
@@ -1,13 +1,11 @@
 def call(){
-	environment {
-                NEXUS_VERSION = "nexus3"
-                NEXUS_PROTOCOL = "https"
-                NEXUS_URL = "nexus.example.com"
-                NEXUS_GROUP = "com.example"
-                NEXUS_CREDENTIAL_ID = "nexus-credentials"
-                MAVEN_SNAPSHOT_REPO = "repo-snapshot"
-                MAVEN_RELEASE_REPO = "repo-release"
-		TARGET = "QA"
-                TEST = """${sh(script: 'echo "$JOB_NAME/test"', returnStdout: true).trim().toLowerCase()}"""
-	}
+                env.NEXUS_VERSION = "nexus3"
+                env.NEXUS_PROTOCOL = "https"
+                env.NEXUS_URL = "nexus.example.com"
+                env.NEXUS_GROUP = "com.example"
+                env.NEXUS_CREDENTIAL_ID = "nexus-credentials"
+                env.MAVEN_SNAPSHOT_REPO = "repo-snapshot"
+                env.MAVEN_RELEASE_REPO = "repo-release"
+		env.TARGET = "QA"
+                env.TEST = """${sh(script: 'echo "$JOB_NAME/test"', returnStdout: true).trim().toLowerCase()}"""
 }
