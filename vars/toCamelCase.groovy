951feabfc3489501a710cbfa6ad210c2df7256c2
Antoinne Williams <antoinne.lamar@gmail.com>
2022-11-20 09:42:33 -0500

    Added class for variable instantiation

--- vars/toCamelCase.groovy
+++
@@ -1,5 +0,0 @@
-def call ( String text, boolean capitalized = false ) {
-    text = text.replaceAll( "(_)([A-Za-z0-9])", { Object[] it -> it[2].toUpperCase() } )
-    return capitalized ? capitalize(text) : text
-}
-
