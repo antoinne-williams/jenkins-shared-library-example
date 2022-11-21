a74f59ccd0f3914eb36a963bcb16c131b4347ac1
Antoinne Williams <antoinne.lamar@gmail.com>
2022-11-21 01:25:20 -0500

    Added method for variable instantiation

---
+++ vars/setEnvVars.groovy
@@ -0,0 +1,26 @@
+def call(target) {
+	if (target == "qa") {
+                env.NEXUS_VERSION = "nexus3"
+                env.NEXUS_PROTOCOL = "https"
+                env.NEXUS_URL = "nexus.example.com"
+                env.NEXUS_GROUP = "com.example"
+                env.NEXUS_CREDENTIAL_ID = "nexus-credentials"
+                env.MAVEN_SNAPSHOT_REPO = "repo-snapshot"
+                env.MAVEN_RELEASE_REPO = "repo-release"
+		env.TARGET = "QA"
+                env.TEST = env.JOB_NAME.trim().toLowerCase().replaceAll("/",":")
+	}
+
+	if (target == "stg") {
+                env.NEXUS_VERSION = "nexus3"
+                env.NEXUS_PROTOCOL = "https"
+                env.NEXUS_URL = "nexus.example.com"
+                env.NEXUS_GROUP = "com.example"
+                env.NEXUS_CREDENTIAL_ID = "nexus-credentials"
+                env.MAVEN_SNAPSHOT_REPO = "repo-snapshot"
+                env.MAVEN_RELEASE_REPO = "repo-release"
+                env.TARGET = "STG"
+                env.TEST = env.JOB_NAME.trim().toLowerCase().replaceAll("/",":")
+	}
+}
+
